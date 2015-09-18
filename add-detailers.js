4b11cb0f8c2e9d0b3d85de1b41fcedb502c29a2e
nannen05 <nannen05@gmail.com>
2015-09-18 14:25:10 -0700

    #4 retuned value from db

--- add-detailers.js
+++ add-detailers.js
@@ -9,7 +9,7 @@ function detailerString () {
   data.firstname = firstname.value;
   data.basicwash = basicWash.value;
   data.superwash = superWash.value;
-  data.dexluewash = deluxeWash.value;
+  data.deluxewash = deluxeWash.value;
   return JSON.stringify(data);
 }
 
@@ -17,7 +17,7 @@ function detailerString () {
 function sendNewDetailer(e) {
   e.preventDefault()
   var xhr = new XMLHttpRequest;
-  xhr.open('POST', '/detailers', true);
+  xhr.open('POST', '/add-detailers', true);
   xhr.setRequestHeader("Content-type","application/json");
   xhr.send(detailerString ());
 }
@@ -26,3 +26,26 @@ var submit = document.getElementById('submit');
 submit.addEventListener('click', function(e){
   sendNewDetailer(e);
 }, false );
+
+function showdb() {
+  //e.preventDefault()
+  var xhr = new XMLHttpRequest;
+  xhr.open('GET', '/show-detailers', true);
+  xhr.setRequestHeader("Content-type","application/json");
+  //xhr.send(null);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var data = xhr.responseText;
+        var jsonResponse = JSON.parse(data);
+        //console.log(jsonResponse[{first_name}]);
+        console.log(data);
+        var showData = document.getElementById('showdb');
+        showData.textContent = jsonResponse[0].first_name;
+      }
+    }
+  };
+  xhr.send(null);
+
+}
+showdb();
