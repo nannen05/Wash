f86b789d6f4156857079a6f6ff6b2cb62d4132f9
nannen05 <nannen05@gmail.com>
2015-09-25 10:22:19 -0700

    #6 broke up page into smaller functions - results block fixed

--- public/main.js
+++ public/main.js
@@ -13,16 +13,6 @@ function sendWashType(e) {
   var targetData = target.getAttribute('data-wash-type');
   globalWashType = targetData;
 }
-function washer () {
-  if(globalWashType == 'Basic')  {
-    return basic;
-  } else if (globalWashType == 'Super'){
-    return superwash;
-  } else if (globalWashType == 'Deluxe') {
-    return deluxe;
-  }
-}
-var wash = washer();
 
 function hideWashType(e){
   var selectedWashType = document.getElementById('selected-wash-type'); //Get Wash Type Id in Cities Block
@@ -57,12 +47,10 @@ selectedcity.addEventListener('click', function(e) {
 }, false);
 
 //Create Column Divs
-var resultDivimage = document.createElement('div');
-var resultDivname = document.createElement('div');
-var resultDivrating = document.createElement('div');
-var resultDivprice = document.createElement('div');
-
-var jsondetailers;
+//var resultDivimage = document.createElement('div');
+//var resultDivname = document.createElement('div');
+//var resultDivrating = document.createElement('div');
+//var resultDivprice = document.createElement('div');
 
 function getdb(callback) {
   var xhr = new XMLHttpRequest;
@@ -76,6 +64,7 @@ function getdb(callback) {
   xhr.send(null);
 };
 
+var jsondetailers;
 getdb(function (result) {
   jsondetailers = result;
 });
@@ -84,11 +73,17 @@ function buildResults() {
   for (var i = 0; i < jsondetailers.length; i++) {
     if (globalCity.value == jsondetailers[i].city) {
 
-      buildImage(i);
-      buildName(i);
-      buildRatings(i);
-      buildPrices(i);
-      blockMaker(i);
+      var resultDivimage = document.createElement('div');
+      var resultDivname = document.createElement('div');
+      var resultDivrating = document.createElement('div');
+      var resultDivprice = document.createElement('div');
+
+      buildImage(i, resultDivimage);
+      buildName(i, resultDivname);
+      buildRatings(i, resultDivrating);
+      buildPrices(i , resultDivprice);
+      blockMaker(resultDivimage, resultDivname, resultDivrating, resultDivprice );
+
 
     } else {
     //console.log('No City Match')
@@ -96,70 +91,73 @@ function buildResults() {
   }
 }
 
-function blockMaker(i) {
+function blockMaker(resultDivimage, resultDivname, resultDivrating, resultDivprice ) {
   var createRow = document.createElement('div');
   createRow.className = 'row result-block';
   startResults.appendChild(createRow);
-  createRow.setAttribute('data-result-number', i);
-  var attr = createRow.attributes['data-result-number'];
-  console.log(attr);
+  //createRow.setAttribute('data-result-number', dbrefrence);
+  //var attr = createRow.attributes['data-result-number'];
+  //console.log(attr);
 
   //Append New blocks To new Row
   createRow.appendChild(resultDivimage);
   createRow.appendChild(resultDivname);
   createRow.appendChild(resultDivrating);
   createRow.appendChild(resultDivprice);
-
 }
 //Create Specific Columns
-function buildImage(i) {
-  createRow.appendChild(resultDivimage);
+function buildImage(dbrefrence, resultDivimage) {
+  var imagePath = "<img src='uploads/" + jsondetailers[dbrefrence].img + "'/>";
   resultDivimage.className = "col-md-3 image";
   var paragraphimage = document.createElement('div');
   resultDivimage.appendChild(paragraphimage);
-  paragraphimage.innerHTML = "<img src='uploads/" + jsondetailers[i].img + "'/>";
+  paragraphimage.innerHTML = imagePath;
+  if (jsondetailers[dbrefrence].img == undefined) {
+    paragraphimage.innerHTML = "<img src='uploads/defaultpic.png'/>";
+  } else {
+  return imagePath;
+  }
 }
-function buildName(i) {
+function buildName(dbrefrence, resultDivname) {
   resultDivname.className = "col-md-3 name";
   var paragraph = document.createElement('h2');
   resultDivname.appendChild(paragraph);
-  paragraph.textContent = jsondetailers[i].first_name;
+  paragraph.textContent = jsondetailers[dbrefrence].first_name;
 }
-function buildRatings(i) {
-  //var starRating = ratings();
+function buildRatings(dbrefrence, resultDivrating) {
+  function ratings() {
+    if (jsondetailers[dbrefrence].rating === 1) {
+      return rating = "<img src='../images/stars.png'>";
+    } else if (jsondetailers[dbrefrence].rating === 2) {
+      return rating = "<img src='../images/stars.png'><img src='../images/stars.png'>";
+    } else if (jsondetailers[dbrefrence].rating === 3) {
+      return rating = "<img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'>";
+    } else if (jsondetailers[dbrefrence].rating === 4) {
+      return rating = "<img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'>";
+    } else {
+      return rating = "<img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'>";
+    }
+  }
+  var starRating = ratings();
   resultDivrating.className = "col-md-3 rating";
   var paragraphrating = document.createElement('p');
   resultDivrating.appendChild(paragraphrating);
-  paragraphrating.innerHTML = jsondetailers[i].rating
+  paragraphrating.innerHTML = starRating;
 }
-function buildPrices (i) {
+function buildPrices (dbrefrence, resultDivprice) {
+   function findWashType() {
+    if(globalWashType == 'Basic')  {
+       return wash = jsondetailers[dbrefrence].basic_wash;
+    } else if (globalWashType == 'Super'){
+       return wash = jsondetailers[dbrefrence].super_wash;
+    } else if (globalWashType == 'Deluxe') {
+     return  wash = jsondetailers[dbrefrence].deluxe_wash;
+    }
+  }
+  var wash = findWashType();
   resultDivprice.className = "col-md-3 price";
   var paragraphprice = document.createElement('p');
   resultDivprice.appendChild(paragraphprice);
-  paragraphprice.textContent = '$'+wash;
+  paragraphprice.textContent = '$'+ wash;
 }
-//function ratings() {
-//  if (rating === 1) {
-//    return rating = "<img src='../images/stars.png'>";
-//  } else if (rating === 2) {
-//    return rating = "<img src='../images/stars.png'><img src='../images/stars.png'>";
-//  } else if (rating === 3) {
-//    return rating = "<img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'>";
-//  } else if (rating === 4) {
-//    return rating = "<img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'>";
-//  } else {
-//    return rating = "<img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'><img src='../images/stars.png'>";
-//  }
-//}
-
-
-
-
-
-
-
-
-
-
-
 
